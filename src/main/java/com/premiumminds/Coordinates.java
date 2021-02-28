fd2ee36b2ec1a5034eb78bfee54f103d8eca7772
Josué Almeida <josue.almeida@gmail.com>
2021-02-28 06:53:52 +0000

    Created Coordinates Class to save X and Y of the current house. Wrote an MVP on the Main Class.

---
+++ src/main/java/com/premiumminds/Coordinates.java
@@ -0,0 +1,27 @@
+package com.premiumminds;
+
+import java.util.Objects;
+
+public class Coordinates {
+
+    long x;
+    long y;
+
+    public Coordinates(long x, long y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinates that = (Coordinates) o;
+        return x == that.x && y == that.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
